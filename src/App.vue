2151a9d81c8dddcc10623d0e9cfbb64385a95d36
rodolphorocha <rodolpho.r2008@gmail.com>
2026-03-31 08:29:55 -0300

    FEAT: Criação feat-1

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,21 @@
 </script>
 
 <template>
+<div class="conteiner-principal">
+<div class="conteiner-secundario">
+  <div class="bem-vindo">
+  <img src="" alt="logo" class="logo">
+  <h1>Bem vindo de volta!</h1>
+  <p>Entre para gerenciar suas finanças</p>
+  </div>
+  <div class="login">
+    <form
+    
+    ></form>
+  </div>
+</div>
 
+</div>
 </template>
 
 <style scoped>
