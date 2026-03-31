a4fc0815cce5f5cf9531ec8b94a05b47e2b1a583
rodolphorocha <rodolpho.r2008@gmail.com>
2026-03-31 08:49:25 -0300

    FEAT: Início do forms

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,23 @@
 </script>
 
 <template>
+<div class="Retornar">
+  <button class="voltar">Voltar para a tela de login</button>
+</div>
+<div class="conteiner">
+  <h1>Criar Conta</h1>
+  <p>Preencha seus dados para começar</p>
 
+
+  <form class="formulario">
+    <input type="text" placeholder="000.000.000-00" required>
+    <input type="text" placeholder="Luiz Fernando Mannrich" required>
+    <input type="text" placeholder="(00) 00000-0000" required>
+    <input type="email" placeholder="Email" required>
+    <input type="password" placeholder="Senha" required>
+    <button type="submit">Criar Conta</button>
+  </form>
+</div>
 </template>
 
 <style scoped>
