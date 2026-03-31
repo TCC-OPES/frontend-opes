dd4580c70300dcdfd854a07f1bc691fc611af57a
rodolphorocha <rodolpho.r2008@gmail.com>
2026-03-31 08:32:26 -0300

    FIX: Correção de bug

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,7 @@
 </script>
 
 <template>
-<div class="conteiner-principal">
-<div class="conteiner-secundario">
-  <div class="bem-vindo">
-  <img src="" alt="logo" class="logo">
-  <h1>Bem vindo de volta!</h1>
-  <p>Entre para gerenciar suas finanças</p>
-  </div>
-  <div class="login">
-    <form
-    
-    ></form>
-  </div>
-</div>
 
-</div>
 </template>
 
 <style scoped>
