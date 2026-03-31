99f6c46475aeeda45260a6b3301e214bf0431f7c
vitoriadesouza <vitoriasouza.ifc@gmail.com>
2026-03-31 16:52:46 -0300

    FEAT: Começo da página de login

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,46 @@
 </script>
 
 <template>
+<section>
+<div class="welcome">
+  <h1>Bem vindo de volta!</h1>
+  <p class="financas">Acesse sua conta e gerencie suas finanças.</p>
+</div>
+<div>
+  <form action=""></form>
+  <p class="email">E-mail</p>
+  <input type="text" placeholder="seu@email.com">
+  <p class="senha">Senha</p>
+  <input type="text" placeholder="•••••••">
+</div>
+<div class="lembrar">
+  <input type="checkbox"> Lembrar-me
+  <a href="">Esqueceu sua senha?</a>
+</div>
+<div class="entrar">
+  <button>Acessar conta</button>
+</div>
+<div class="criar">
+  <p>Não tem uma conta? <a href="">Criar conta</a> </p>
+</div>
 
+
+
+</section>
 </template>
 
 <style scoped>
+p.financas {
+  margin: 0 0 30px 0;
 
+}
+input {
+  padding: 10px;
+  border-radius: 10px;
+  border: 1px solid;
+  margin: 10px 0 10px 0;
+}
+.lembrar a {
+  color: red;
+}
 </style>
