58498c976dbb9ae1e6a07b8eeaf1856ae7eda67a
rodolphorocha <rodolpho.r2008@gmail.com>
2026-04-17 21:55:11 -0300

    feat: alterações na HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <script setup>
-
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
   <main>
+    <RouterLink to="/login">Entrar</RouterLink>
+    <RouterLink to="/cadastro">Comece Grátis</RouterLink>
+
+
   </main>
 </template>
