1c4e5de08bd880f491948f246be47490c76aa835
rodolphorocha <rodolpho.r2008@gmail.com>
2026-07-09 01:08:20 -0300

    feat: acredito que coloquei a logo no app

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bqps302ttnk"
+    "revision": "0.cpiukgshp0s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
