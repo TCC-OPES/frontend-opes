c7f5351dc355008caa55aeb49b5a798a9a7bd734
vitormsantanaa <vitormanoelssz1704@gmail.com>
2026-08-26 11:17:36 -0300

    FIX: Arrumei as coisas la da issue e tambem a sidebar

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-290dd570'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r37ck096n34"
+    "revision": "0.dj9jrbcqp9s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
